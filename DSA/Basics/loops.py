f023f08e8743a5ade173011e568c1d04c6b8fc26
Mona <mona.mk.agrawal@gmail.com>
2026-09-10 13:28:47 +0530

    Progress

---
+++ DSA/Basics/loops.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    print(i)
+    
+#starting from another number
+
+for i in range(9,18,2):
+    print(i)
+    
+    
+#printing tables
+
+for i in range(1,11):
+    print(7*i)
---
+++ DSA/Basics/takingInput.py
@@ -0,0 +1,2 @@
+name1, name2 = input("Enter two names please: ").split()
+print(name1 , "Marries", name2)
